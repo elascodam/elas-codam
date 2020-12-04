2be420267ed607f4e4bf50ee0f4a2deb92325c18
Luma Arruda <luma.oarruda@gmail.com>
2020-12-04 14:17:36 -0300

    text corrections

--- src/componets/IntroElasCodam/introText.js
+++ src/componets/IntroElasCodam/introText.js
@@ -1,4 +1,4 @@
-const paragraph1 = `Somos um coletivo de mulheres que estudam programação e que atuam no mercado de tecnologia, atuando juntas por um mundo melhor e mais inclusivo com`
+const paragraph1 = `Somos um coletivo de mulheres que estudam programação e que atuam no mercado de tecnologia, atuando juntas por um mundo melhor e mais inclusivo.`
 
 const paragraph2 = `Um grupo de mulheres que acreditam umas nas outras!`
 
