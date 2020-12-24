978ec6d640954069e9972f1e21216fca6431dc95
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2020-12-24 11:17:56 -0300

    sobre elas

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import { PageTemplate } from "../../componets";
-import { Pages } from "../../Constants";
+import { Pages, SobreElasText } from "../../Constants";
+import {
+  Paragraph,
+  Span,
+  Title,
+  TextContainer
+} from './styles';
 
 const AboutUs = () => (
   <PageTemplate page={Pages.SobreElas}>
-    <h1>Sobre nós</h1>
+    <Title fontSize="60px">
+      <Span fontSize="60px">&lt;</Span>
+      {SobreElasText.Title}
+      <Span fontSize="60px">&gt;</Span>
+    </Title>
+    <TextContainer>
+      <Paragraph fontSize="40px">{SobreElasText.Paragraph1}</Paragraph>
+    </TextContainer>
   </PageTemplate>
 );
 
