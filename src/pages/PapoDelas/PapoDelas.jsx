983d58f89616985024857476242763c77721ba5b
isabellavjs <isabellavjs@gmail.com>
2021-03-21 12:15:56 -0300

    Adiciona textos para PapoDelas

--- src/pages/PapoDelas/PapoDelas.jsx
+++ src/pages/PapoDelas/PapoDelas.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { PageTemplate } from "../../componets";
+import { Paragraph } from "../../componets/IntroElasCodam/styles";
 import { Pages, PapoDelasText } from "../../Constants";
 import {
   Title,
   Span,
+  TextContainer,
 } from './styles';
 
 const PapoDelas = () => (
@@ -13,6 +15,10 @@ const PapoDelas = () => (
     {PapoDelasText.Title}
     <Span fontSize="60px">&gt;</Span>
     </Title>
+    <TextContainer>
+      <Paragraph fontSize="40px">{PapoDelasText.Paragraph1}</Paragraph>
+      <Paragraph fontSize="40px">{PapoDelasText.Paragraph2}</Paragraph>
+    </TextContainer>
   </PageTemplate>
 );
 
--- src/pages/PapoDelas/styles.js
+++ src/pages/PapoDelas/styles.js
@@ -43,3 +43,42 @@ color: var(--SOFT_ORANGE);
     font-size: 12px;
   }
 `;
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: #7D49AA;
+
+  @media (max-width: 1100px) {
+    display: flex;
+    justify-content: center;
+  }
+
+  @media (max-width: 400px) {
+    display: flex;
+    justify-content: center;
+    margin: 0 auto;
+    width: 40vw;
+}
+`
+
+export const Paragraph = styled.p`
+  color: var(--SOFT_ORANGE);
+  font-size: ${(props) => (props.fontSize ? props.fontSize : "20px")};
+  text-align: center;
+  margin: 70px 150px;
+
+  @media (max-width: 1100px) {
+    font-size: 20px;
+  }
+
+  @media (max-width: 800px) {
+    font-size: 16px;
+    width: 40vw;
+  }
+
+  @media (max-width: 400px) {
+    font-size: 12px;
+  }
+`
