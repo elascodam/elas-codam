03b2545ab817197d9e6994f3dc29d7678e393c6f
isabellavjs <isabellavjs@gmail.com>
2021-03-21 12:07:39 -0300

    Define os styles para PapoDelas

--- src/pages/PapoDelas/PapoDelas.jsx
+++ src/pages/PapoDelas/PapoDelas.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { PageTemplate } from "../../componets";
+import { Pages, PapoDelasText } from "../../Constants";
+import {
+  Title,
+  Span,
+} from './styles';
 
 const PapoDelas = () => (
-  <PageTemplate>
-    <h1>PapoDelas</h1>
+  <PageTemplate page={Pages.PapoDelas}>
+    <Title fontSize="60px">
+    <Span fontSize="60px">&lt;</Span>
+    {PapoDelasText.Title}
+    <Span fontSize="60px">&gt;</Span>
+    </Title>
   </PageTemplate>
 );
 
--- src/pages/PapoDelas/styles.js
+++ src/pages/PapoDelas/styles.js
@@ -1,1 +1,45 @@
 // aqui vai o styled components do papo delas
+import styled from "styled-components";
+
+export const Title = styled.span`
+color: var(--VIOLET);
+  font-size: ${(props) => (props.fontSize ? props.fontSize : "20px")};
+  text-align: center;
+
+  display: flex;
+  margin: 0px 0px 80px 0px;
+  justify-content: center;
+
+  @media (max-width: 1100px) {
+    font-size: 20px;
+  }
+
+  @media (max-width: 800px) {
+    font-size: 16px;
+    width: 40vw;
+  }
+
+  @media (max-width: 400px) {
+    font-size: 12px;
+  }
+`;
+
+export const Span = styled.span`
+color: var(--SOFT_ORANGE);
+  font-size: ${(props) => (props.fontSize ? props.fontSize : "20px")};
+  text-align: center;
+  margin: 0px 5px;
+
+  @media (max-width: 1100px) {
+    font-size: 20px;
+  }
+
+  @media (max-width: 800px) {
+    font-size: 16px;
+    width: 40vw;
+  }
+
+  @media (max-width: 400px) {
+    font-size: 12px;
+  }
+`;
