5a93135dd8f13f05f07636393db365a6695cb21e
isabellavjs <isabellavjs@gmail.com>
2021-03-21 11:53:20 -0300

    Adiciona constante PapoDelasText

--- src/Constants.js
+++ src/Constants.js
@@ -34,3 +34,12 @@ export const SobreElasText = {
   Paragraph1:
   "Somos um coletivo de mulheres que estudam programação e que atuam no mercado de tecnologia. Esse grupo surgiu de uma iniciativa de alunas da Trybe, mas está aberto à todas as mulheres que se interessem em participar."
 }
+
+export const PapoDelasText = {
+  Title:
+  "PapoDelas",
+  Paragraph1:
+  "O PapoDelas é um evento onde recebemos uma convidada para debates e apresentações sobre a área de tecnologia e também sobre temáticas sociais, como machismo e transfobia.",
+  Paragraph2:
+  "Com esse evento visamos criar um espaço para que mulheres que queiram compartilhar seus conhecimentos se conectem com aquelas que estão interessadas em aprender, disseminando conhecimento e aumentando a representatividade no mercado tecnológico."
+}
